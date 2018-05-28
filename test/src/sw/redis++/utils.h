fde20342a1b97913f04f6eadca0c84bd85d9f157
sewenew <sewe.code@gmail.com>
2018-05-28 23:48:34 +0800

    add KeyDeleter helper, and throw exception if redis_assert failled.

--- test/src/sw/redis++/utils.h
+++ test/src/sw/redis++/utils.h
@@ -17,7 +17,9 @@
 #ifndef SEWENEW_REDISPLUSPLUS_TEST_UTILS_H
 #define SEWENEW_REDISPLUSPLUS_TEST_UTILS_H
 
-#include <iostream>
+#include <string>
+#include <vector>
+#include <sw/redis++/redis++.h>
 
 #define REDIS_ASSERT(condition, msg) \
     sw::redis::test::redis_assert((condition), (msg), __FILE__, __LINE__)
@@ -33,13 +35,41 @@ inline void redis_assert(bool condition,
                             const std::string &file,
                             int line) {
     if (!condition) {
-        std::cerr << "ASSERT: " << msg << ". " << file << ":" << line << std::endl;
+        auto err_msg = "ASSERT: " + msg + ". " + file + ":" + std::to_string(line);
+        throw Error(err_msg);
     }
 }
 
 inline std::string test_key(const std::string &k) {
-    return "sw:redis:test:" + k;
+    return "sw::redis::test::" + k;
 }
+
+class KeyDeleter {
+public:
+    template <typename Input>
+    KeyDeleter(Redis &redis, Input first, Input last) : _redis(redis), _keys(first, last) {
+        _delete();
+    }
+
+    KeyDeleter(Redis &redis, std::initializer_list<std::string> il) :
+                KeyDeleter(redis, il.begin(), il.end()) {}
+
+    KeyDeleter(Redis &redis, const std::string &key) : KeyDeleter(redis, {key}) {}
+
+    ~KeyDeleter() {
+        _delete();
+    }
+
+private:
+    void _delete() {
+        if (!_keys.empty()) {
+            _redis.del(_keys.begin(), _keys.end());
+        }
+    }
+
+    Redis &_redis;
+    std::vector<std::string> _keys;
+};
 
 }
 
