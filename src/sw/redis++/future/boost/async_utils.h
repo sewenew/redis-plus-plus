c60cc77a6f9e6666cefca494bd3fd4e0829a4034
fantasy-peak <1356346239@qq.com>
2022-03-25 10:26:33 +0800

    Add support c++20 coroutine

--- src/sw/redis++/future/boost/async_utils.h
+++ src/sw/redis++/future/boost/async_utils.h
@@ -17,6 +17,13 @@
 #ifndef SEWENEW_REDISPLUSPLUS_ASYNC_UTILS_H
 #define SEWENEW_REDISPLUSPLUS_ASYNC_UTILS_H
 
+#ifdef __cpp_impl_coroutine
+
+#define BOOST_THREAD_PROVIDES_EXECUTORS
+#define BOOST_THREAD_USES_MOVE
+
+#endif
+
 #define BOOST_THREAD_PROVIDES_FUTURE
 #define BOOST_THREAD_PROVIDES_FUTURE_CONTINUATION
 
@@ -32,8 +39,109 @@ using Future = boost::future<T>;
 template <typename T>
 using Promise = boost::promise<T>;
 
-}
+} // namespace redis
 
-}
+} // namespace sw
+
+#ifdef __cpp_impl_coroutine
+
+#include <coroutine>
+
+#include <boost/thread/executors/basic_thread_pool.hpp>
+
+namespace sw {
+
+namespace redis {
+
+template <typename T>
+struct FutureAwaiter {
+    FutureAwaiter(boost::future<T>&& future)
+        : _future(std::move(future)) {
+    }
+
+    FutureAwaiter(boost::future<T>&& future, boost::executors::basic_thread_pool* pool_ptr)
+        : _future(std::move(future))
+        , _pool_ptr(pool_ptr) {
+    }
+
+    bool await_ready() { return false; }
+    void await_suspend(std::coroutine_handle<> handle) {
+        auto func = [this, handle](sw::redis::Future<T> fut) mutable {
+            try {
+                _result = fut.get();
+            } catch (...) {
+                try {
+                    boost::rethrow_exception(fut.get_exception_ptr());
+                } catch (const std::exception_ptr& ep) {
+                    _exception = ep;
+                }
+            }
+            handle.resume();
+        };
+        if (_pool_ptr != nullptr)
+            _future.then(*_pool_ptr, std::move(func));
+        else
+            _future.then(std::move(func));
+    }
+
+    T await_resume() {
+        if (_exception)
+            std::rethrow_exception(_exception);
+        return std::move(_result);
+    }
+
+    boost::future<T> _future;
+    boost::executors::basic_thread_pool* _pool_ptr{nullptr};
+    std::decay_t<decltype(std::declval<boost::future<T>>().get())> _result;
+    std::exception_ptr _exception{nullptr};
+};
+
+template <>
+struct FutureAwaiter<void> {
+    FutureAwaiter(boost::future<void>&& future)
+        : _future(std::move(future)) {
+    }
+
+    FutureAwaiter(boost::future<void>&& future, boost::executors::basic_thread_pool* pool_ptr)
+        : _future(std::move(future))
+        , _pool_ptr(pool_ptr) {
+    }
+
+    bool await_ready() { return false; }
+    void await_suspend(std::coroutine_handle<> handle) {
+        auto func = [this, handle](sw::redis::Future<void> fut) mutable {
+            try {
+                fut.wait();
+            } catch (...) {
+                try {
+                    boost::rethrow_exception(fut.get_exception_ptr());
+                } catch (const std::exception_ptr& ep) {
+                    _exception = ep;
+                }
+            }
+            handle.resume();
+        };
+        if (_pool_ptr != nullptr)
+            _future.then(*_pool_ptr, std::move(func));
+        else
+            _future.then(std::move(func));
+    }
+
+    void await_resume() {
+        if (_exception)
+            std::rethrow_exception(_exception);
+        return;
+    }
+
+    boost::future<void> _future;
+    boost::executors::basic_thread_pool* _pool_ptr{nullptr};
+    std::exception_ptr _exception{nullptr};
+};
+
+} // namespace redis
+
+} // namespace sw1
+
+#endif
 
 #endif // end SEWENEW_REDISPLUSPLUS_ASYNC_UTILS_H
--- src/sw/redis++/future/std/async_utils.h
+++ src/sw/redis++/future/std/async_utils.h
@@ -29,8 +29,90 @@ using Future = std::future<T>;
 template <typename T>
 using Promise = std::promise<T>;
 
-}
+} // namespace redis
 
-}
+} // namespace sw
+
+#ifdef __cpp_impl_coroutine
+
+#include <coroutine>
+namespace sw {
+
+namespace redis {
+
+class Executor {
+public:
+    Executor() = default;
+    virtual ~Executor() = default;
+
+    virtual void enqueue(std::function<void()>) = 0;
+};
+
+template <typename T>
+struct FutureAwaiter {
+    FutureAwaiter(std::future<T>&& future, Executor* executor_ptr)
+        : _future(std::move(future))
+        , _executor_ptr(executor_ptr) {
+    }
+
+    bool await_ready() { return false; }
+    void await_suspend(std::coroutine_handle<> handle) {
+        _executor_ptr->enqueue([this, handle] {
+            try {
+                _result = _future.get();
+            } catch (const std::exception&) {
+                _exception = std::current_exception();
+            }
+            handle.resume();
+        });
+    }
+
+    T await_resume() {
+        if (_exception)
+            std::rethrow_exception(_exception);
+        return std::move(_result);
+    }
+
+    std::future<T> _future;
+    Executor* _executor_ptr{nullptr};
+    std::decay_t<decltype(std::declval<std::future<T>>().get())> _result;
+    std::exception_ptr _exception{nullptr};
+};
+
+template <>
+struct FutureAwaiter<void> {
+    FutureAwaiter(std::future<void>&& future, Executor* executor_ptr)
+        : _future(std::move(future))
+        , _executor_ptr(executor_ptr) {
+    }
+
+    bool await_ready() { return false; }
+    void await_suspend(std::coroutine_handle<> handle) {
+        _executor_ptr->enqueue([this, handle] {
+            try {
+                _future.wait();
+            } catch (const std::exception&) {
+                _exception = std::current_exception();
+            }
+            handle.resume();
+        });
+    }
+
+    void await_resume() {
+        if (_exception)
+            std::rethrow_exception(_exception);
+        return;
+    }
+
+    std::future<void> _future;
+    Executor* _executor_ptr{nullptr};
+    std::exception_ptr _exception{nullptr};
+};
+
+} // namespace redis
+
+} // namespace sw
+
+#endif
 
 #endif // end SEWENEW_REDISPLUSPLUS_ASYNC_UTILS_H
