fe6e72fd32bf75c0f3fea8213e7062f609b9b40c
sewenew <sewe.code@gmail.com>
2024-03-17 23:35:41 +0800

    add version info to source code

---
+++ src/sw/redis++/version.h
@@ -0,0 +1,32 @@
+/**************************************************************************
+   Copyright (c) 2024 sewenew
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ *************************************************************************/
+
+#ifndef SEWENEW_REDISPLUSPLUS_VERSION_H
+#define SEWENEW_REDISPLUSPLUS_VERSION_H
+
+namespace sw {
+
+namespace redis {
+
+const int VERSION_MAJOR = 1;
+const int VERSION_MINOR = 3;
+const int VERSION_PATCH = 13;
+
+}
+
+}
+
+#endif // end SEWENEW_REDISPLUSPLUS_VERSION_H
